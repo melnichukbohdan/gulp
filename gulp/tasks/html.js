af7f193c3853572c1a3ad955b69a95b1f155cdf2
Bohdan Melnychuk <b.melnychuk@dev-branch.com>
2024-07-10 15:42:59 +0300

    Added Web Html NoSvg plugin

--- gulp/tasks/html.js
+++ gulp/tasks/html.js
@@ -1,8 +1,10 @@
 // The Gulp task for build index.html file.
 import fileInclude from "gulp-file-include";
+import webHtmlNoSvg from 'gulp-webp-html-nosvg'
 export const html = () => {
   return app.gulp.src(app.path.src.html)
     .pipe(fileInclude())
     .pipe(app.plugins.replace(/@img\//g, 'img/'))
+    .pipe(webHtmlNoSvg())
     .pipe(app.gulp.dest(app.path.build.html))
 }
