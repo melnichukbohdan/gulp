8e690c74c463bec4ec4a07fc4292cf71b3fbd099
Bohdan Melnychuk <b.melnychuk@dev-branch.com>
2024-07-10 15:30:28 +0300

    Added HTML files compiler and the plugin for alias replacement

---
+++ gulp/config/plugins.js
@@ -0,0 +1,6 @@
+// The replace plugin
+import replace from "gulp-replace";
+
+export const plugins = {
+  replace: replace
+}
--- gulpfile.js
+++ gulpfile.js
@@ -5,20 +5,28 @@ import {path} from "./gulp/config/path.js";
 // Import gulp tasks
 import {copy} from "./gulp/tasks/copy.js";
 import {reset} from "./gulp/tasks/reset.js";
+import {html} from "./gulp/tasks/html.js";
+
+// Import plugins.
+import {plugins} from "./gulp/config/plugins.js";
 
 // Add values to the global App variable.
 global.app = {
   path: path,
   gulp: gulp,
+  plugins: plugins,
 }
 
 // Create the gulp watcher.
 function watcher() {
   gulp.watch(path.watch.files, copy)
+  gulp.watch(path.watch.html, html)
 }
 
+const mainTask = gulp.parallel(copy, html)
+
 // Gulp dev script
-const dev = gulp.series(reset, copy, watcher);
+const dev = gulp.series(reset, mainTask, watcher);
 
 // Execution of the default script.
 gulp.task('default', dev);
